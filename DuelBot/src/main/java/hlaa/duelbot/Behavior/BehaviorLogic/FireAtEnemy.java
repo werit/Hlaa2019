8eeddb30508ae8b1df1e5e28f5f8b63963c4dab7
werit <tib.balaz@gmail.com>
2019-09-13 10:26:21 +0200

    Added new Behavior

---
+++ DuelBot/src/main/java/hlaa/duelbot/Behavior/BehaviorLogic/FireAtEnemy.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2019 AMIS research group, Faculty of Mathematics and Physics, Charles University in Prague, Czech Republic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package hlaa.duelbot.Behavior.BehaviorLogic;
+
+import hlaa.duelbot.Behavior.BehaviorResource;
+import hlaa.duelbot.Behavior.BotCapabilities;
+import hlaa.duelbot.Behavior.ConditionDto;
+
+/**
+ *
+ * @author msi
+ */
+public class FireAtEnemy implements IBehavior {
+
+    private double priority;
+    private BehaviorResource behaviorResource;
+    private ConditionDto conditionDto;
+
+    public FireAtEnemy(double priority, BehaviorResource behaviorResource) {
+        this.priority = priority;
+        this.behaviorResource = behaviorResource;
+        // Todo
+        conditionDto = new ConditionDto(true, true, false);
+    }
+
+    @Override
+    public BotCapabilities[] GetBotCapabilities() {
+        return new BotCapabilities[]{BotCapabilities.FIRE};
+    }
+
+    @Override
+    public IBehavior Stop() {
+        behaviorResource.shoot.stopShooting();
+        return null;
+    }
+
+    @Override
+    public IBehavior Execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean IsUsable(ConditionDto currentlyMetConditionDto) {
+        return conditionDto.AreConditionsMetAtCurrentRun(currentlyMetConditionDto);
+    }
+
+    @Override
+    public double GetPriority() {
+        return this.priority;
+    }
+
+}
